45e4ef33b3ded3f6957f212ff03df5469870de07
KaranSinghWebdeveloper <156217780+KaranSinghWebdeveloper@users.noreply.github.com>
2024-12-25 19:04:26 +0530

    Update db.js
    fs

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config();
 
 const uri = process.env.DB;
 
-mongoose.connect(uri)
+mongoose.createConnection(uri)
     .then(() => console.log('Connected to MongoDB Atlas'))
     .catch(err => console.log('Error connecting to MongoDB:', err));
 
