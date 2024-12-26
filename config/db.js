665fa7eac358cf00cedbd65243817942799cb614
KaranSinghWebdeveloper <156217780+KaranSinghWebdeveloper@users.noreply.github.com>
2024-12-26 21:17:42 +0530

    Update db.js
    aa

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
-const uri = process.env.DB;
+const uri = 'mongodb+srv://karanSingh:FkFkaCUdSQ7iUPcF@cluster0.n6n0m.mongodb.net/nodeApp?retryWrites=true&w=majority';
 
 mongoose.connect(uri)
     .then(() => console.log('Connected to MongoDB Atlas'))
